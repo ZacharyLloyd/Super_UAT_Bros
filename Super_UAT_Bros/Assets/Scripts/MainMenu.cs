1d5db7f7d81c16d9a93ae4d2fc7b6b8b3a70a6c1
ZacharyLloyd <46659989+ZacharyLloyd@users.noreply.github.com>
2019-03-12 20:27:17 -0700

    Setting up the game

---
+++ Super_UAT_Bros/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    //Play the game or play again
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+    //Quit the game
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Super_UAT_Bros/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed; //Speed for moving
+    public float maxSpeed; //Max Speed
+    public float jump; // Amount of jump
+    [HideInInspector] public bool isWalking; // Is player walking true/false
+    Animator animator; // Player's animator
+    Rigidbody2D rb; // Player's rigidbody
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
